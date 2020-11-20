f3f57a7612e9cefe5a87f351a3cbea5249e00f2b
Elena Ditiateva <elena2091@bk.ru>
2020-11-20 18:17:51 +0500

    fix header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,41 +1,43 @@
-import React from 'react';
-import Logo from './Logo';
-import Menu from './Menu';
-import ShowButton from './ShowButton';
-import SlideMenu from './SlideMenu';
+import React from "react";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import ShowButton from "./ShowButton";
+import SlideMenu from "./SlideMenu";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: false
-        }
-        this.toggleMenu = this.toggleMenu.bind(this);
-    }
+  toggleMenu() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
 
-    toggleMenu() {
-        this.setState({
-            visible: !this.state.visible
-        });
-    }
+  render() {
+    return (
+      //<div className={`container ${title ? "bg-secondary" : "bg-transparent"}`}>
 
-    render() {
-        return( //<div className={`container ${title ? "bg-secondary" : "bg-transparent"}`}>
+      <header className="z-10 absolute top-0 left-0 w-full py-8">
         <div className="container">
-            <header className="z-10 absolute top-0 left-0 w-full py-8">
-                <div className="flex justify-between items-center px-10">
-                    <Logo/>
-                    <Menu/>
-                    <ShowButton handleClick={this.toggleMenu} />
-                    <div id="container">
-                        <SlideMenu handleClick={this.toggleMenu} visibility={this.state.visible} />
-                    </div>
-                </div>
-            </header>
+          <div className="flex justify-between items-center px-10">
+            <Logo />
+            <Menu />
+            <ShowButton handleClick={this.toggleMenu} />
+            <SlideMenu
+              handleClick={this.toggleMenu}
+              visibility={this.state.visible}
+            />
+          </div>
         </div>
-        );
-    }
+      </header>
+    );
+  }
 }
 
 export default Header;
