79fc91d88e447b56fc70193f99aa6c8d7137a523
Ksu <newksyuha@gmail.com>
2020-11-20 20:20:11 +0200

    1

--- client/src/components/Infrastructure/InfrastructureItem.jsx
+++ client/src/components/Infrastructure/InfrastructureItem.jsx
@@ -5,15 +5,15 @@ function InfrastructureItem() {
     return (
         <div className="flex flex-row ">
 
-            <div className="w-1/2 p-2 h-auto">
+            <div className="w-1/2 p-2 mb-10 h-auto">
                 <div className="bistro img"></div>
                 <div className="text-2xl p-4">ATLAS Bistro</div>
-                <div className="text-lg p-2">Ресторан неофранцузской кухни</div>
+                <div className="text-lg">Ресторан неофранцузской кухни</div>
             </div>
-            <div className="w-1/2 p-2 h-auto">
+            <div className="w-1/2 p-2 mb-10 h-auto">
                 <div className="parking"></div>
                 <div className="text-2xl p-4">Подземный паркинг</div>
-                <div className="text-lg p-2">Просторный отапливаемый паркинг на 67 машиномест с доступом на этажи</div>
+                <div className="text-lg">Просторный отапливаемый паркинг на 67 машиномест с доступом на этажи</div>
             </div>
         </div>
     );
