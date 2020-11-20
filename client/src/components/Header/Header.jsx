5628ea849bf6a58d1ce5e7934d4dc162681c1d94
Elena Ditiateva <elena2091@bk.ru>
2020-11-20 23:50:27 +0500

    change Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -20,22 +20,41 @@ class Header extends React.Component {
   }
 
   render() {
+    const title = this.props.title;
+    const smalltitle = this.props.smalltitle;
+    const maintitle = this.props.maintitle;
+    const city = this.props.city;
     return (
-      //<div className={`container ${title ? "bg-secondary" : "bg-transparent"}`}>
-
-      <header className="z-10 absolute top-0 left-0 w-full py-8">
-        <div className="container">
-          <div className="flex justify-between items-center px-10">
-            <Logo />
-            <Menu />
-            <ShowButton handleClick={this.toggleMenu} />
-            <SlideMenu
-              handleClick={this.toggleMenu}
-              visibility={this.state.visible}
-            />
+      <div className="relative w-full flex items-end bg-no-repeat bg-cover bg-center SectionBg">
+        <header className="z-10 absolute top-0 left-0 w-full py-8 bg-secondary">
+          <div className="container">
+            <div className="flex justify-between items-center">
+              <Logo />
+              <Menu />
+              <ShowButton handleClick={this.toggleMenu} />
+              <SlideMenu
+                handleClick={this.toggleMenu}
+                visibility={this.state.visible}
+              />
+            </div>
           </div>
+          <div className={`w-full ${title ? "bg-secondary" : "bg-transparent"}`}>
+            <div className="container">
+              <div className="py-16">
+                {title && <h4 className="text-white text-5xl">{title}</h4>}
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <div className="container flex flex-col text-white pb-20">
+          <h4 className="mb-6 text-xs uppercase font-bold leading-5 tracking-wide smalltitle">
+            {smalltitle}
+          </h4>
+          <h1 className="maintitle">{maintitle}</h1>
+          <h1 className="city mb-20">{city}</h1>
         </div>
-      </header>
+      </div>
     );
   }
 }
