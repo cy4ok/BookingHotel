ce69748355279a2509329c76ca1da87b4a3e5d18
igogggg <64080406+igogggg@users.noreply.github.com>
2020-11-21 16:44:28 +0300

    remove no needs component

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -4,8 +4,7 @@ import Discounts from "../components/Discounts";
 import Header from "../components/Header/Header";
 import Offer from "../components/Offer";
 import CorpOffer from "../components/CorpOffer/CorpOffer"
-import RestorauntHero from "../components/Restaurant/RestaurantHero"
-import RestorauntSection from "../components/Restaurant/RestaurantSection"
+
 const MainPage = () => {
   return (
     <>
@@ -14,8 +13,7 @@ const MainPage = () => {
       <ArtStudio />
       <CorpOffer/>
       <Discounts title="Текущие акции" />
-      <RestorauntHero/>
-      <RestorauntSection/>
+      
     </>
   );
 };
