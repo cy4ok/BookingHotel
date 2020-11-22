a34c940ed085db6e09b86bf5952344f6cc535804
igogggg <64080406+igogggg@users.noreply.github.com>
2020-11-22 11:37:38 +0300

    Merge remote-tracking branch 'upstream/main' into restaurant

--- client/src/pages/Restaurant.js
+++ client/src/pages/Restaurant.js
@@ -2,12 +2,14 @@ import React from 'react';
 import RestaurantHero from "../components/Restaurant/RestaurantHero";
 import RestaurantSection from "../components/Restaurant/RestaurantSection"
 import HeaderAll from "../components/Restaurant/HeaderAll.jsx"
+import Footer from "../components/Footer/"
 function Restaurant() {
     return (
         <div className="page_wrap relative">
             <HeaderAll/>
             <RestaurantHero/>
             <RestaurantSection/>
+            <Footer/>
         </div>
     );
 }
