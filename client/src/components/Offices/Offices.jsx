e48e973505229562ac1b75058cf898b5b84f0722
Svetlana Efimova <svetlana.e08@gmail.com>
2020-11-24 13:46:14 +0300

    fix Offices

--- client/src/components/Offices/Offices.jsx
+++ client/src/components/Offices/Offices.jsx
@@ -13,13 +13,6 @@ const Offices = () => {
                 Развитая инфраструктура, удобная транспортная доступность.
             </p>
             <p className="text-xl font-bold mb-6">Арендаторам мы предлагаем:</p>
-            {/* <ul className="my-6">
-                <li className="clients_sections_list relative pl-6 mt-4">Круглосуточный доступ в офис;</li>
-                <li className="clients_sections_list relative pl-6 mt-4">24/7 служба ресепшн;</li>
-                <li className="clients_sections_list relative pl-6 mt-4">“Переложить” бытовые вопросы на администратора ARTSTUDIO Nevsky;</li>
-                <li className="clients_sections_list relative pl-6 mt-4">Подземный паркинг с доступом на этаж;</li>
-                <li className="clients_sections_list relative pl-6 mt-4">Собственная служба безопасности и видеонаблюдение</li>
-            </ul> */}
             {OfficesListData.map((d, i) => (
                 <OfficesListItem text={d} key={i} />
             ))}
