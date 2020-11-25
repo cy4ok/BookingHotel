03f91eb244610b0caa95c5e4f6e95a7a8a693544
Konfeta <konfeta333@gmail.com>
2020-11-25 19:16:50 +0300

    WIP Add Search section

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,16 +3,22 @@ import ArtStudio from "../components/ArtStudio";
 import Discounts from "../components/Discounts";
 import Header from "../components/Header/Header";
 import Offer from "../components/Offer";
-import CorpOffer from "../components/CorpOffer/CorpOffer"
-import Footer from "../components/Footer"
+import CorpOffer from "../components/CorpOffer/CorpOffer";
+import Footer from "../components/Footer";
+import Search from "../components/Search/Search";
 
 const MainPage = () => {
   return (
     <>
-      <Header bgHeader="header-bg-1" smallTitle="ДОМ ВДАЛИ ОТ ДОМА" mainTitle="ARTSTUDIO - апарт-отель в Санкт-Петербурге"/>
+      <Header
+        bgHeader="header-bg-1"
+        smallTitle="ДОМ ВДАЛИ ОТ ДОМА"
+        mainTitle="ARTSTUDIO - апарт-отель в Санкт-Петербурге"
+      />
+      <Search />
       <Offer />
       <ArtStudio />
-      <CorpOffer/>
+      <CorpOffer />
       <Discounts title="Текущие акции" />
       <Footer />
     </>
