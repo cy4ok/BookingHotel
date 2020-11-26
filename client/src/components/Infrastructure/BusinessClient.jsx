f68ed798a0519d35f2e944cc6dfc95ff731608da
Ksu <newksyuha@gmail.com>
2020-11-26 15:30:50 +0200

    fix color

--- client/src/components/Infrastructure/BusinessClient.jsx
+++ client/src/components/Infrastructure/BusinessClient.jsx
@@ -9,9 +9,9 @@ function BusinessClient() {
             <div className="text-white w-1/2 p-16">
                 <h1 className="text-5xl mb-2">Бизнес-клиентам</h1>
                 <div className="text-lg">Приезжаете в Петербург по работе? Мы позаботимся о том, чтобы ваше проживание было максимально комфортным. Все бытовые вопросы мы возьмем на себя, обеспечив вас необходимыми сервисами: высокоскоростной проводной и беспроводной Wi Fi, мини-офис, трансфер, услуги личного помощника. Мы также можем организовать для вас рабочее место в коворкинге или арендовать переговорную комнату. Мы расположены в непосредственной близости от большинства офисных центров, а также офисов Администрации Петербурга.</div>
-                <div className="text-left mt-8 btn">
+                <div className="text-left mt-8 hover:text-brown">
                     <Link to="/to-clients">
-                        <span className="text-lg text-left btn">
+                        <span className="text-lg text-left">
                             <LinkIcon className="btn-linicon mr-3 mb-1" />
                         Предложение для бизнес-клиентов
                 </span>
--- client/src/components/Infrastructure/InfrastructureItem.jsx
+++ client/src/components/Infrastructure/InfrastructureItem.jsx
@@ -11,12 +11,12 @@ function InfrastructureItem() {
                 <div className="text-2xl text-brown">
                 <Link to="/restaurant">ATLAS Bistro</Link>
                     </div>
-                <div className="text-lg textGray py-2">Ресторан неофранцузской кухни</div>
+                <div className="text-lg text-grey textOpacity py-2">Ресторан неофранцузской кухни</div>
             </div>
             <div className="w-1/2 h-auto">
                 <div className="parking bg-no-repeat bg-cover mb-8"></div>
                 <div className="text-2xl ">Подземный паркинг</div>
-                <div className="text-lg textGray py-2">Просторный отапливаемый паркинг на 67 машиномест с доступом на этажи</div>
+                <div className="text-lg text-grey textOpacity py-2">Просторный отапливаемый паркинг на 67 машиномест с доступом на этажи</div>
             </div>
         </div>
     );
