141dcf974be6f78a1611a9f801e1c8cc28ec18f1
Konfeta <konfeta333@gmail.com>
2020-11-26 16:26:12 +0300

    WIP Add guest field

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -6,10 +6,46 @@ import Calendar from "./Calendar";
 const Search = () => {
   const { handleSubmit, errors, control } = useForm();
   const onSubmit = (data) => console.log(data);
+
   const [hidden, setHidden] = useState(true);
 
   const hideGuests = () => {
     setHidden(!hidden);
+  };
+
+  const [adult, setAdult] = useState(0);
+  const [children, setChildren] = useState(0);
+  let countAdult = adult;
+  let countChildren = children;
+
+  const increase = (type, event) => {
+    if (type === "adult") {
+      countAdult++;
+      setAdult(countAdult);
+      event.stopPropagation();
+    }
+    if (type === "children") {
+      countChildren++;
+      setChildren(countChildren);
+      event.stopPropagation();
+    }
+  };
+
+  const decrease = (type, event) => {
+    if (type === "adult" && adult > 0) {
+      countAdult--;
+      setAdult(countAdult);
+      event.stopPropagation();
+    } else {
+      event.stopPropagation();
+    }
+    if (type === "children" && children > 0) {
+      countChildren--;
+      setChildren(countChildren);
+      event.stopPropagation();
+    } else {
+      event.stopPropagation();
+    }
   };
 
   return (
@@ -32,30 +68,84 @@ const Search = () => {
           >
             Гости
             <div
-              className={`flex flex-col absolute search-guests mt-5 p-5 w-72 left-0 right-0 z-30 bg-white text-grey ${
+              className={`flex flex-col absolute search-guests pointer-events-none mt-5 p-5 w-72 left-0 right-0 z-30 bg-white text-grey ${
                 hidden ? "hidden" : "flex"
               }`}
             >
-              <div className="flex w-full border-black">
+              <div className="flex w-full">
                 <div className="w-2/3">Взрослые</div>
-                <div className="flex w-1/3">
-                  <div className="flex justify-center items-center text-2xl search-btn-circle cursor-pointer bg-btnGold text-white">
+                <div className="flex w-1/3 pointer-events-auto">
+                  <div
+                    className={`flex justify-center items-center text-2xl search-btn-circle outline-none bg-btnGold text-white ${
+                      adult === 0
+                        ? "opacity-50 cursor-default"
+                        : "opacity - 100"
+                    }`}
+                    onClick={(event) => {
+                      decrease("adult", event);
+                    }}
+                  >
                     -
                   </div>
-                  <input className="w-16" />
-                  <div className="flex justify-center items-center text-2xl search-btn-circle cursor-pointer bg-btnGold text-white">
+                  {/* <input
+                    name="plus"
+                    control={control}
+                    ref={register}
+                    defaultValue={count}
+                    className="w-16"
+                  /> */}
+                  {/* <Controller
+                    as={<input />}
+                    name="plus"
+                    control={control}
+                    defaultValue={count}
+                    className="w-16"
+                  /> */}
+                  <input className="w-16 text-center" value={adult} disabled />
+                  {/* <button
+                    className="flex justify-center items-center text-2xl search-btn-circle outline-none search-btn-circle-plus cursor-pointer bg-btnGold text-white"
+                    onClick={(event) => {
+                      increase("adult", event);
+                    }}
+                  >
+                    +
+                  </button> */}
+                  <div
+                    className="flex justify-center items-center text-2xl search-btn-circle outline-none search-btn-circle-plus cursor-pointer bg-btnGold text-white"
+                    onClick={(event) => {
+                      increase("adult", event);
+                    }}
+                  >
                     +
                   </div>
                 </div>
               </div>
-              <div className="flex w-full border-black mt-5">
+              <div className="flex w-full mt-5">
                 <div className="w-2/3">Дети</div>
-                <div className="flex w-1/3">
-                  <div className="flex justify-center items-center text-2xl search-btn-circle cursor-pointer bg-btnGold text-white">
+                <div className="flex w-1/3 pointer-events-auto">
+                  <div
+                    className={`flex justify-center items-center text-2xl search-btn-circle outline-none bg-btnGold text-white ${
+                      children === 0
+                        ? "opacity-50 cursor-default"
+                        : "opacity - 100"
+                    }`}
+                    onClick={(event) => {
+                      decrease("children", event);
+                    }}
+                  >
                     -
                   </div>
-                  <input className="w-16" />
-                  <div className="flex justify-center items-center text-2xl search-btn-circle cursor-pointer bg-btnGold text-white">
+                  <input
+                    className="w-16 text-center"
+                    value={children}
+                    disabled
+                  />
+                  <div
+                    className="flex justify-center items-center text-2xl search-btn-circle outline-none search-btn-circle-plus cursor-pointer bg-btnGold text-white"
+                    onClick={(event) => {
+                      increase("children", event);
+                    }}
+                  >
                     +
                   </div>
                 </div>
