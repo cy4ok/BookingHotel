f7685c8bc769763c297c97d485fe94ae1d02343c
Ksu <newksyuha@gmail.com>
2020-11-27 18:39:43 +0200

    fix

--- client/src/pages/PolicyPage.js
+++ client/src/pages/PolicyPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Policy from "../components/Policy"
 
@@ -9,7 +8,6 @@ const PolicyPage = () => {
     <>
       <Header bgHeader="header-bg-2"  mainTitle="Политика конфиденциальности" />
       <Policy />
-      <Footer />
     </>
   );
 };
