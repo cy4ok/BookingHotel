fd2fc6b4b093db7ae57945b5d5a9e99f2b642b8d
igogggg <64080406+igogggg@users.noreply.github.com>
2020-12-14 22:37:05 +0300

    delete footer to TermOfUsePage

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Route path="/discounts" component={DiscountsPage} />
           <Route path="/about" component={AboutPage} />
           {/* <Route path="/about" component={About} /> */}
-          <Route path="/polisy" component={PolicyPage} />
+          <Route path="/policy" component={PolicyPage} />
           <Route exact path="/contacts" component={ContactsPage} />
           <Route exact path="/offices" component={OfficesPage} />
           <Route exact path="/termsofuse" component={TermsOfUsePage} />
