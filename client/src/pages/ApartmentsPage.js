eac6d8de9a7a7d40702a06d3d802a42dfc60771b
Ksu <newksyuha@gmail.com>
2020-12-19 15:41:12 +0200

    Вернула привязки раздела инфраструктуры

--- client/src/pages/ApartmentsPage.js
+++ client/src/pages/ApartmentsPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
-/* import Infrastructure from "../components/Infrastructure"; */
-import Superior from "../components/NumberCard/Superior/Superior.jsx";
+import Infrastructure from "../components/Infrastructure";
+
 
 
 const ApartmentsPage = () => {
   return (
     <>
       <Header bgHeader="header-bg-2" smallTitle="от 2 800 руб." mainTitle="ARTSTUDIO Nevsky" address="ул. 2-ая Советская, 4Б, стр.1"/>
-      {/* <Infrastructure /> */}
-      <Superior />
+      <Infrastructure />
+      
     </>
   );
 };
