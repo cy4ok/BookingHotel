6d6c4815c9a15e4df66335479dc0ab31f17db3fe
Elena Ditiateva <elena2091@bk.ru>
2020-12-19 19:51:48 +0300

    fix header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -31,7 +31,7 @@ class Header extends React.Component {
         style={bgHeader ? { height: "620px" } : { height: "320px" }}
       >
         <header
-          className="z-10 absolute top-0 left-0 w-full py-8 bg-secondary"
+          className="z-100 absolute top-0 left-0 w-full py-8 bg-secondary"
           style={
             bgHeader
               ? {
